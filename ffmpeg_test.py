6fc71e436eab7c238a70d068660024d43424c7c9
LongHip12 <longhip2012@gmail.com>
2025-10-03 12:31:59 +0700

    Update ffmpeg_test.py

--- ffmpeg_test.py
+++ ffmpeg_test.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 import subprocess
+import os
 
+# Kiểm tra vị trí ffmpeg
 result = subprocess.run(["which", "ffmpeg"], capture_output=True, text=True)
 print("FFmpeg path:", result.stdout.strip())
-# Keep Bot Alive
 
-app = Flask(__name__)
+# Khai báo Flask và set template_folder = thư mục hiện tại
+app = Flask(__name__, template_folder=os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")  # giờ có thể load trực tiếp file cùng folder
 
 if __name__ == "__main__":
-    # debug=True để tự reload khi sửa code
-    app.run(host='0.0.0.0', port=5000, debug=True)
-  
+    app.run(host="0.0.0.0", port=5000, debug=True)
